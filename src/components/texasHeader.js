7e7e3a1d28f008e472283729f9ce65a46ac1c76d
Alex Reyes <alexanderfreues@email.arizona.edu>
2020-08-03 13:02:56 -0700

    Adding the rest of the added files

---
+++ src/components/texasHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function TexasHeader() {
+  
+  return (
+    <div>
+        <img src="/tx-logo.png" className = "logo" style={{width: 50}} alt='The logo of Texas' />
+        
+        <br/>
+        <br/>
+        <hr/>
+    </div>
+  );
+}
+
+export default TexasHeader;
---
+++ src/components/texasSeal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function TexasSeal() {
+  
+  return (
+    <img src="/tx-seal.png" style={{width: 150}} alt='The seal of Texas' />
+  );
+}
+
+export default TexasSeal;
